a04a2e1000b86c5ec464975bf81abd7fbe6ad66f
Eduardo Ansa <eduardoansa@gmail.com>
2022-03-08 20:45:43 -0300

    Pre-deploiment

---
+++ components/layout/MainLayout.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+import styles from './MainLayout.module.css';
+import{FC} from 'react';
+
+export const MainLayout: FC = ({ children }) => {
+    return (
+        <div className={styles.container}>
+          
+          <Head>
+            <title>Home - Eduardo</title>
+            <meta name="description" content="Home Page" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <Navbar />
+    
+          <main className={styles.main}>
+    
+            { children }
+    
+          </main>     
+        </div>
+      )
+};
---
+++ pages/about.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { MainLayout } from '../components/layout/MainLayout'
+import {DarkLayout} from '../components/layout/DarkLayout'
+
+export default function About() {
+    return (
+      <>
+       
+          <h1>About Page</h1>
+
+          <h1 className="title">
+          ir a <Link href="/" >Home</Link>
+          </h1>
+
+          <p className={"description"}>
+            Get started by editing{' '}
+            <code className={"code"}>pages/about.jsx</code>
+          </p>
+      </>
+    )
+}
+
+About.getLayout = function getLayout (page: JSX.Element) {
+  return(
+    <MainLayout>
+      <DarkLayout>
+        {page}
+      </DarkLayout>
+    </MainLayout>
+  )
+}
